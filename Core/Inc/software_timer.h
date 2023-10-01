1f26f67301acd20c1119a580748d294551a2a45d
NhatAnh1801 <anh.vuong0110@hcmut.edu.vn>
2023-10-01 11:04:12 +0700

    Ex1

---
+++ Core/Inc/software_timer.h
@@ -0,0 +1,14 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Oct 1, 2023
+ *      Author: ADMIN
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer1_flag;
+void setTimer(int duration);
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
